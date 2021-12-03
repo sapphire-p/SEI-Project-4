c6269f68a894793aa05140d5184f8d4f22042799
Sapphire Paston <sapphire.paston@gmail.com>
2021-12-03 21:43:14 +0000

    views.py for plants built out with routes for GET all, GET single, POST new plant, PUT update single plant, DELETE single plant. Tested in Insomnia and working correctly

--- plants/views.py
+++ plants/views.py
@@ -19,6 +19,14 @@ class PlantListView(APIView):
         serialized_plants = PlantSerializer(plants, many=True)
         return Response(serialized_plants.data, status=status.HTTP_200_OK)
 
+    def post(self, request):
+        plant = PlantSerializer(data=request.data)
+        if plant.is_valid():
+            plant.save()
+            return Response(plant.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(plant.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 # ? For requests made to /plants/pk/
 class PlantDetailView(APIView):
@@ -30,3 +38,23 @@ class PlantDetailView(APIView):
             return Response({'message': 'Plant not found'}, status=status.HTTP_404_NOT_FOUND)
         serialized_plant = PlantSerializer(plant)
         return Response(serialized_plant.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        try:
+            plant = Plant.objects.get(id=pk)
+        except:
+            return Response({'message': 'Plant not found'}, status=status.HTTP_404_NOT_FOUND)
+        updated_plant = PlantSerializer(plant, data=request.data)
+        if updated_plant.is_valid():
+            updated_plant.save()
+            return Response(updated_plant.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(updated_plant.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+    def delete(self, request, pk):
+        try:
+            plant = Plant.objects.get(id=pk)
+            plant.delete()
+        except:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(status=status.HTTP_204_NO_CONTENT)
