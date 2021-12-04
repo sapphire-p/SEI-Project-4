50345e09f2b40e7503c95bf5aca6ff7d1bc8aadb
Sapphire Paston <sapphire.paston@gmail.com>
2021-12-04 11:17:04 +0000

    View for Reviews POST request to create new review set up and tested in Insomnia, working as expected

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 from .models import Review
+
+# from jwt_auth.serializers import UserSerializer
 # from jwt_auth.serializers import NonRegistrationUserSerializer
+# ! from jwt_auth.models import User
 
 
 class ReviewSerializer(serializers.ModelSerializer):
