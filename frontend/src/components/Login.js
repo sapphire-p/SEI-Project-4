e0b4eebbe2d8483ee576b00b1c4ec8f5261bc7d8
Sapphire Paston <sapphire.paston@gmail.com>
2021-12-10 10:16:42 +0000

    Hero added to Register and Login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -48,30 +48,41 @@ const Login = () => {
 
 
   return (
-    <Container style={{ width: '50%', minWidth: '490px', maxWidth: '650px' }}>
-      <div className='m-5'>
-        <h1 className='mb-5'>Login</h1>
-        <Form onSubmit={handleSubmit}>
-
-          <Form.Group className='mb-3' controlId='formUsername'>
-            <Form.Label className='font-weight-bold'>Username</Form.Label>
-            <Form.Control type='text' placeholder='Enter username' name='username' value={formData.username} onChange={handleChange} />
-          </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formPassword'>
-            <Form.Label className='font-weight-bold'>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' name='password' value={formData.password} onChange={handleChange} />
-            {error && <Form.Text className='text-danger'>Invalid login credentials</Form.Text>}
-          </Form.Group>
-
-          <div className='d-flex justify-content-center'>
-            <Button variant='primary' type='submit' className='bg-success mt-1 mb-3' style={{ width: '100%', borderStyle: 'none' }}>
-              Submit
-            </Button>
-          </div>
-        </Form>
+    <>
+      <div id='hero' className="jumbotron jumbotron-fluid">
+        <Container className="text-center">
+          <h1 className="display-5 font-weight-bold">Login</h1>
+          <p className="lead mt-4">
+            Welcome! Enter your details below to log in <br />
+            Not signed up yet? Click &apos;Register&apos; above to create your profile
+          </p>
+        </Container>
       </div>
-    </Container >
+      <Container style={{ width: '50%', minWidth: '490px', maxWidth: '650px' }}>
+        <div className='m-5'>
+          {/* <h1 className='mb-5'>Login</h1> */}
+          <Form onSubmit={handleSubmit}>
+
+            <Form.Group className='mb-3' controlId='formUsername'>
+              <Form.Label className='font-weight-bold'>Username</Form.Label>
+              <Form.Control type='text' placeholder='Enter username' name='username' value={formData.username} onChange={handleChange} />
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formPassword'>
+              <Form.Label className='font-weight-bold'>Password</Form.Label>
+              <Form.Control type='password' placeholder='Password' name='password' value={formData.password} onChange={handleChange} />
+              {error && <Form.Text className='text-danger'>Invalid login credentials</Form.Text>}
+            </Form.Group>
+
+            <div className='d-flex justify-content-center'>
+              <Button variant='primary' type='submit' className='bg-success mt-1 mb-3' style={{ width: '100%', borderStyle: 'none' }}>
+                Login
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Container >
+    </>
   )
 
 }
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -62,89 +62,100 @@ const Register = () => {
 
 
   return (
-    <Container style={{ width: '55%', minWidth: '490px', maxWidth: '650px' }}>
-      <div className='m-5'>
-        <h1 className='mb-5'>Register</h1>
-        <Form onSubmit={handleSubmit}>
-
-          <Form.Group className='mb-3' controlId='formUsername'>
-            <Form.Label className='font-weight-bold'>Username</Form.Label>
-            <Form.Text className='text-muted pb-2'>
-              Once registered, you will need your username and password to login
-            </Form.Text>
-            <Form.Control type='text' placeholder='Enter username' name='username' value={formData.username} onChange={handleChange} />
-            <Form.Text className='text-danger'>
-              {errors.username ? errors.username[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formBasicEmail'>
-            <Form.Label className='font-weight-bold'>Email address</Form.Label>
-            <Form.Control type='email' placeholder='Enter email' name='email' value={formData.email} onChange={handleChange} />
-            <Form.Text className='text-danger'>
-              {errors.email ? errors.email[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formPassword'>
-            <Form.Label className='font-weight-bold'>Password</Form.Label>
-            <Form.Control type='password' placeholder='Password' name='password' value={formData.password} onChange={handleChange} />
-            <Form.Text className='text-danger'>
-              {errors.password ? errors.password[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formPasswordConfirmation'>
-            <Form.Label className='font-weight-bold'>Password Confirmation</Form.Label>
-            <Form.Control type='password' placeholder='Password confirmation' name='password_confirmation' value={formData.password_confirmation} onChange={handleChange} />
-            <Form.Text className='text-danger'>
-              {errors.password_confirmation ? errors.password_confirmation[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formThreeWordBio'>
-            <Form.Label className='font-weight-bold'>Three Word Bio</Form.Label>
-            <Form.Control type='text' placeholder='Describe yourself in just three words!' name='three_word_bio' value={formData.three_word_bio} onChange={handleChange} />
-            <Form.Text className='text-danger'>
-              {errors.three_word_bio ? errors.three_word_bio[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group className='mb-3' controlId='formAboutMe'>
-            <Form.Label className='font-weight-bold'>About Me</Form.Label>
-            <Form.Text className='text-muted pb-2'>
-              We&apos;d love to hear about your plant collection, hobbies, favourite plant...
-            </Form.Text>
-            <textarea className='form-control' id="exampleFormControlTextarea1" rows="4" type='text' placeholder='Tell us about yourself in a couple of sentences' name='about_me' value={formData.about_me} onChange={handleChange}></textarea>
-            <Form.Text className='text-danger'>
-              {errors.about_me ? errors.about_me[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <Form.Group controlId='formFile' className='mb-3'>
-            <Form.Label className='font-weight-bold'>Upload a profile image</Form.Label>
-            <Form.Text className='text-muted pb-3'>
-              Our uploader recognises faces, so a headshot works best. <br />
-              If you don&apos;t upload a profile image, we&apos;ll give you a default one :)
-            </Form.Text>
-            <ImageUploadField
-              value={formData.profile_image}
-              name='profile_image'
-              handleImageUrl={handleImageUrl}
-            />
-            <Form.Text className='text-danger'>
-              {errors.profile_image ? errors.profile_image[0] : ''}
-            </Form.Text>
-          </Form.Group>
-
-          <div className='d-flex justify-content-center'>
-            <Button variant='primary' type='submit' className='bg-success mt-1 mb-3' style={{ width: '100%', borderStyle: 'none' }}>
-              Submit
-            </Button>
-          </div>
-        </Form>
+    <>
+      <div id='hero' className="jumbotron jumbotron-fluid">
+        <Container className="text-center">
+          <h1 className="display-5 font-weight-bold">Register</h1>
+          <p className="lead mt-4">
+            Sign up to create your own profile, save plants and leave reviews<br />
+            Members also enjoy access to our growing community
+          </p>
+        </Container>
       </div>
-    </Container >
+      <Container style={{ width: '55%', minWidth: '490px', maxWidth: '650px' }}>
+        <div className='m-5'>
+          {/* <h1 className='mb-5'>Register</h1> */}
+          <Form onSubmit={handleSubmit}>
+
+            <Form.Group className='mb-3' controlId='formUsername'>
+              <Form.Label className='font-weight-bold'>Username</Form.Label>
+              <Form.Text className='text-muted pb-2'>
+                Once registered, you will need your username and password to login
+              </Form.Text>
+              <Form.Control type='text' placeholder='Enter username' name='username' value={formData.username} onChange={handleChange} />
+              <Form.Text className='text-danger'>
+                {errors.username ? errors.username[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formBasicEmail'>
+              <Form.Label className='font-weight-bold'>Email address</Form.Label>
+              <Form.Control type='email' placeholder='Enter email' name='email' value={formData.email} onChange={handleChange} />
+              <Form.Text className='text-danger'>
+                {errors.email ? errors.email[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formPassword'>
+              <Form.Label className='font-weight-bold'>Password</Form.Label>
+              <Form.Control type='password' placeholder='Password' name='password' value={formData.password} onChange={handleChange} />
+              <Form.Text className='text-danger'>
+                {errors.password ? errors.password[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formPasswordConfirmation'>
+              <Form.Label className='font-weight-bold'>Password Confirmation</Form.Label>
+              <Form.Control type='password' placeholder='Password confirmation' name='password_confirmation' value={formData.password_confirmation} onChange={handleChange} />
+              <Form.Text className='text-danger'>
+                {errors.password_confirmation ? errors.password_confirmation[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formThreeWordBio'>
+              <Form.Label className='font-weight-bold'>Three Word Bio</Form.Label>
+              <Form.Control type='text' placeholder='Describe yourself in just three words!' name='three_word_bio' value={formData.three_word_bio} onChange={handleChange} />
+              <Form.Text className='text-danger'>
+                {errors.three_word_bio ? errors.three_word_bio[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className='mb-3' controlId='formAboutMe'>
+              <Form.Label className='font-weight-bold'>About Me</Form.Label>
+              <Form.Text className='text-muted pb-2'>
+                We&apos;d love to hear about your plant collection, hobbies, favourite plant...
+              </Form.Text>
+              <textarea className='form-control' id="exampleFormControlTextarea1" rows="4" type='text' placeholder='Tell us about yourself in a couple of sentences' name='about_me' value={formData.about_me} onChange={handleChange}></textarea>
+              <Form.Text className='text-danger'>
+                {errors.about_me ? errors.about_me[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group controlId='formFile' className='mb-3'>
+              <Form.Label className='font-weight-bold'>Upload a profile image</Form.Label>
+              <Form.Text className='text-muted pb-3'>
+                Our uploader recognises faces, so a headshot works best. <br />
+                If you don&apos;t upload a profile image, we&apos;ll give you a default one :)
+              </Form.Text>
+              <ImageUploadField
+                value={formData.profile_image}
+                name='profile_image'
+                handleImageUrl={handleImageUrl}
+              />
+              <Form.Text className='text-danger'>
+                {errors.profile_image ? errors.profile_image[0] : ''}
+              </Form.Text>
+            </Form.Group>
+
+            <div className='d-flex justify-content-center'>
+              <Button variant='primary' type='submit' className='bg-success mt-1 mb-3' style={{ width: '100%', borderStyle: 'none' }}>
+                Submit
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </Container >
+    </>
   )
 }
 
